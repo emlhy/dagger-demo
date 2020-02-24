81c443a6ddaec406ccf682b25195904bbffff3f0
Hongyi <emlhy66@gmail.com>
2020-02-24 16:51:00 -0500

    first retrofit call

--- app/src/main/java/com/emlhy/daggerdemo/network/auth/AuthApi.java
+++ app/src/main/java/com/emlhy/daggerdemo/network/auth/AuthApi.java
@@ -1,11 +1,17 @@
 package com.emlhy.daggerdemo.network.auth;
 
+import com.emlhy.daggerdemo.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
 
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(
+            @Path("id") int id
+    );
 }
--- app/src/main/java/com/emlhy/daggerdemo/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/emlhy/daggerdemo/ui/auth/AuthViewModel.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.emlhy.daggerdemo.models.User;
 import com.emlhy.daggerdemo.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -19,11 +24,29 @@ public class AuthViewModel extends ViewModel {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: viewmodel is working...");
 
-        if(this.authApi == null){
-            Log.d(TAG, "AuthViewModel: auth api is NULL");
-        }
-        else {
-            Log.d(TAG, "AuthViewModel: auth api is NOT NULL");
-        }
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(User user) {
+                Log.d(TAG, "onNext: " + user.getEmail());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onNext: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
